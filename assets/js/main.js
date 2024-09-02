c27c09dc140884f37389ff1ad4d2e8bca67803db
SurajVishw <surajvishwakarma10234@gmail.com>
2024-09-02 22:30:51 +0530

    Updated Sticky Funtionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,45 +1,48 @@
-'use strict';
+"use strict";
 
-let notesInput = document.querySelector('.notes-input');
-let addNotesBtn = document.querySelector('.add-notes');
-let stickyContainer = document.querySelector('.sticyknotes-container');
+let notesInput = document.querySelector(".notes-input");
+let addNotesBtn = document.querySelector(".add-notes");
+let stickyContainer = document.querySelector(".sticyknotes-container");
 
-addNotesBtn.addEventListener('click', () => {
-    let notesValue = notesInput.value;
-    let stickyNotes = document.querySelector('.sticky-notes');
-    let errorEle = document.createElement('p');
+addNotesBtn.addEventListener("click", () => {
+  let notesValue = notesInput.value;
+  let stickyNotes = document.querySelector(".sticky-notes");
+  let errorEle = document.querySelector(".error-msg");
 
-    if (!notesValue) {
-        errorEle.classList.add('error-msg');
-        errorEle.textContent = "Please Enter a Notes";
-        stickyNotes.appendChild(errorEle);
-    } else {
-        let stickyList = document.createElement('div');
-        let stickyPara = document.createElement('p');
-        let stickyCloseBtn = document.createElement('button');
+  if (errorEle) {
+    errorEle.remove();
+  }
 
-        stickyContainer.appendChild(stickyList);
-        stickyList.appendChild(stickyPara);
-        stickyList.appendChild(stickyCloseBtn);
+  if (!notesValue) {
+    let errorEle = document.createElement("p");
+    errorEle.classList.add("error-msg");
+    errorEle.textContent = "Please Enter a Notes";
+    stickyNotes.appendChild(errorEle);
+  } else {
+    let stickyList = document.createElement("div");
+    let stickyPara = document.createElement("p");
+    let stickyCloseBtn = document.createElement("button");
 
-        stickyList.classList.add('stickynotes-list');
-        stickyPara.classList.add('stickyenterd-content');
-        stickyCloseBtn.classList.add('delete-notes');
+    stickyContainer.insertBefore(stickyList, stickyContainer.children[0]);
+    stickyList.appendChild(stickyPara);
+    stickyList.appendChild(stickyCloseBtn);
 
-        stickyPara.textContent = notesValue;
-        stickyCloseBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+    stickyList.classList.add("stickynotes-list");
+    stickyPara.classList.add("stickyenterd-content");
+    stickyCloseBtn.classList.add("delete-notes");
+
+    stickyPara.textContent = notesValue;
+    stickyCloseBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                             viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
                             stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x">
                             <path d="M18 6 6 18" />
                             <path d="m6 6 12 12" />
                         </svg>`;
 
+    stickyCloseBtn.addEventListener("click", function () {
+      stickyList.remove();
+    });
 
-        stickyCloseBtn.addEventListener('click', function () {
-            stickyList.remove()
-        })
-
-        notesInput.value = "";
-        errorEle.remove();
-    }
+    notesInput.value = "";
+  }
 });
